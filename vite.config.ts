860cfe283577af391b63b38674f4fd4898bd8e01
Andrei Babkin <bandit.tcpc@gmail.com>
2025-10-16 13:24:41 +0300

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: process.env.NODE_ENV === 'production' ? '/my-vue-app/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/form-events/' : '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
